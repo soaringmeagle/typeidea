2124a822db891eb57613f05d7f014d5e228705f6
soaringmeagle <yeah_wangfei@163.com>
2021-02-11 10:21:04 +0800

    拆分settings

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -70,15 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'typeidea.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -103,9 +94,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
