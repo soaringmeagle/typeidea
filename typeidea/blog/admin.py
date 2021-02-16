390a91918a4769da8322342562499a7dbac756c3
soaringmeagle <yeah_wangfei@163.com>
2021-02-16 17:34:35 +0800

    添加日志查看

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -6,6 +6,7 @@ from .models import Post, Category, Tag
 from .adminforms import PostAdminForm
 from typeidea.custom_site import custom_site
 from typeidea.base_admin import BaseOwnerAdmin
+from django.contrib.admin.models import LogEntry
 
 
 # Register your models here.
@@ -134,3 +135,8 @@ class PostAdmin(BaseOwnerAdmin):
     #         'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",),
     #     }
     #     js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
+
+
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id', 'action_flag', 'user', 'change_message']
