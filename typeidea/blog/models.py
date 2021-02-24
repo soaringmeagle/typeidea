2fb9b67909d4c01700ce3861adbac5502c776b6b
soaringmeagle <yeah_wangfei@163.com>
2021-02-24 21:13:13 +0800

    重构post_list视图

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -64,6 +64,32 @@ class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoseNotExist:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+        return post_list, tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            category = None
+            post_list = []
+        else:
+            post_list = category.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner', 'category')
+        return post_list, category
+
+    @classmethod
+    def latest_posts(cls):
+        query_set = cls.ojects.filter(status=cls.STATUS_NORMAL)
+
     def __str__(self):
         return self.title
 
